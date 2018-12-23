66a2496354a5c1d4cdd51df8f16a08a9495bbcf6
ReedwanHossain <reedwanhossan16@gmail.com>
2018-12-23 17:39:27 +0600

    documentation added

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,52 @@
 ( function ( window ) {
 
     'use strict'
+    
+    let temp;
+    
+    function postAjax(url, data, success) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open('POST', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    return xhr;
+}
+
+
+
 
     function BkoiDev( ) {
 
         var Bkoi = { };
 
-        Bkoi.search = function ( query ) {
+        Bkoi.search = function ( query, cb ) {
+          
+          postAjax('https://barikoi.xyz/v1/tnt/search/test/', {  search: query }, function(response){cb(response) });
 
-            axios.post( 'https://barikoi.xyz/v1/tnt/search/test/', {
-                search: query,
-              })
+            // axios.post( 'https://barikoi.xyz/v1/tnt/search/test/', {
+            //     search: query,
+            //   })
 
-              .then( function ( response ) {
-                console.log( response.data.places );
-              })
+            //   .then( function ( response ) {
+            //     // console.log(response.data.places);
+            //     cb(response.data.places)
+            //   })
 
-              .catch( function ( error ) {
-                console.log( error );
-              })
+            //   .catch( function ( error ) {
+            //     console.log( error );
+            //   });
+              
+              // return function(response){
+              //     return response
+              // }
 
         }
 

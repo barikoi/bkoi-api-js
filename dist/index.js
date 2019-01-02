a6243efe8e5c5bfee7bf3464fb392d4580eebe1a
Bappi Zaman <asadzaman666@outlook.com>
2019-01-02 16:44:22 +0600

    bkoi-dev 0.1

--- dist/index.js
+++ dist/index.js
@@ -1,89 +1,166 @@
-(function(window) {
+(function (window) {
 
     'use strict'
 
-    var scripts = document.getElementsByTagName('script');
-    var myScript = scripts[scripts.length - 1];
-
-    var queryString = myScript.src.replace(/^[^\?]+\??/, '');
-    console.log(queryString)
-
-    var params = parseQuery(queryString);
-    console.log(params)
+    var scripts = document.getElementsByTagName('script')
+    var myScript = scripts[scripts.length - 1]
+    var queryString = myScript.src.replace(/^[^\?]+\??/, '')
+    var params = parseQuery(queryString)
 
     function parseQuery(query) {
-        var Params = new Object();
-        if (!query) return Params; // return empty object
-        var Pairs = query.split(/[;&]/);
+
+        var Params = new Object()
+
+        if (!query) return Params // return empty object
+
+        var Pairs = query.split(/[;&]/)
+
         for (var i = 0; i < Pairs.length; i++) {
-            var KeyVal = Pairs[i].split(':');
-            if (!KeyVal || KeyVal.length != 2) continue;
-            var key = unescape(KeyVal[0]);
-            var val = unescape(KeyVal[1]);
-            val = val.replace(/\+/g, ' ');
-            Params[key] = val;
+
+            var KeyVal = Pairs[i].split(':')
+
+            if (!KeyVal || KeyVal.length != 2) continue
+
+            var key = unescape(KeyVal[0])
+            var val = unescape(KeyVal[1])
+            val = val.replace(/\+/g, ' ')
+
+            Params[key] = val
         }
-        return Params;
+
+        return Params
     }
 
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 
     function postAjax(url, data, success) {
-        var params = typeof data == 'string' ? data : Object.keys(data).map(
-            function(k) {
-                return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-            }
-        ).join('&');
 
-        // var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-        xhr.open('post', url);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState > 3 && xhr.status == 200) {
-                success(xhr.responseText);
+        var params = typeof data == 'string' ? data : Object.keys(data).map(
+
+            function (k) {
+
+                return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+
             }
-        };
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send(params);
-        return xhr;
+
+        ).join('&')
+
+        // var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP")
+
+        xhr.open('post', url)
+        xhr.onreadystatechange = function () {
+
+            if (xhr.readyState > 3 && xhr.status == 200) {
+                success(xhr.responseText)
+            }
+        }
+
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        xhr.send(params)
+
+        return xhr
     }
 
     function httpGetAsync(theUrl, callback) {
 
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200)
-                callback(xhr.responseText);
-        }
-        xhr.open("GET", theUrl, true); // true for asynchronous 
-        xhr.send(null);
-        return xhr;
-    }
+        xhr.onreadystatechange = function () {
 
+            if (xhr.readyState == 4 && xhr.status == 200)
+
+                callback(xhr.responseText)
+        }
+
+        xhr.open("GET", theUrl, false) // true for asynchronous 
+        xhr.send(null)
+
+        return xhr
+
+    }
 
 
     function BkoiDev() {
 
-        var Bkoi = {};
+        if (params.key) {       //API key check
 
-        Bkoi.search = function(query, cb) {
+            var Bkoi = {}
 
-            httpGetAsync('https://barikoi.xyz/v1/api/search/autocomplete/' + params.key + '/place?q=' + query, function(response) {
-                
-                if (Array.isArray(JSON.parse(response).places)) {
-                   return cb(JSON.parse(response).places)
-                }
-                 
-                return cb([])
-            });
+            // REVERSE GEOCODING //
 
+            Bkoi.reverseGeo = function (longitude, latitude, cb) {
+
+                let reverseGeoUrl = 'https://barikoi.xyz/v1/api/search/reverse/geocode/' + params.key + '/place?' +
+                    'longitude=' + longitude + '&latitude=' + latitude
+
+                httpGetAsync(reverseGeoUrl, function (response) {
+
+                    cb(response)
+
+                })
+            }
+
+            // GEOCODING //
+
+            Bkoi.geoInfo = function (place_id, cb) {
+
+                let geoUrl = 'https://barikoi.xyz/v1/api/search/geocode/' + params.key + '/place/' + place_id
+
+                httpGetAsync(geoUrl, function (response) {
+
+                    cb(response)
+
+                })
+            }
+
+
+            // AUTOCOMPLETE //
+
+            Bkoi.search = function (query, cb) {
+
+                let searchUrl = 'https://barikoi.xyz/v1/api/search/autocomplete/' + params.key + '/place?q=' + query
+
+                httpGetAsync(searchUrl, function (response) {
+
+                    if (Array.isArray(JSON.parse(response).places)) {
+
+                        return cb(JSON.parse(response).places)
+
+                    }
+
+                    return cb([])
+                });
+
+            }
+
+
+            // NEARBY //
+
+            Bkoi.nearby = function (longitude, latitude, cb) {
+
+                let nearbyUrl = 'https://barikoi.xyz/v1/api/search/nearby/' + params.key + '/0.5/10?' +
+                    'longitude=' + longitude + '&latitude=' + latitude
+
+                httpGetAsync(nearbyUrl, function (response) {
+
+                    if (Array.isArray(JSON.parse(response))) {
+                        return cb(JSON.parse(response))
+                    }
+
+                    return cb([])
+                })
+            }
+
+            return Bkoi;
+
+        } else {
+            console.log( ' MURI KHAW ')
         }
-
-        return Bkoi;
-
     }
 
-    if (typeof(Bkoi) === 'undefined') {
+    if (typeof (Bkoi) === 'undefined') {
+
         window.Bkoi = new BkoiDev()
+
     }
 
-}(window));
+}(window))
